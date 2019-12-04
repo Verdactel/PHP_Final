0accb477b7f0cda06a5ee7e6369b5c404c43fa1a
Verdactel <36209800+Verdactel@users.noreply.github.com>
2019-12-04 10:45:26 -0700

    Update index.php

--- Front-End/index.php
+++ Front-End/index.php
@@ -30,7 +30,7 @@
             }
     }
     else{
-        if(isset($_SESSION['UserID'])){
+        if(isset($_SESSION['userID'])){
             echo "Hello ".$_SESSION['UserName']."!";
             // echo "Hello ".$_SESSION["username"];
 
@@ -74,18 +74,6 @@
             }
         }
         else{
-            //default display
-            //generate search bars if necessary
-
-            if(isset($_SESSION['userID'])){
-                echo $_SESSION['userID'];
-                echo "Hello ".$_SESSION["username"];
-                
-                //display a certain way
-                //call database to find interests
-                //display based on interests
-            }
-            else{
                 //default display
                 $response = file_get_contents('https://newsapi.org/v2/top-headlines?country=us&apiKey=c1bffb35cf8d439fa9cd84b0c4d535e6');
                 $data = json_decode($response);
@@ -99,7 +87,7 @@
                 foreach($data->articles as $articles){
                     //check for null image first
                 echo '<a style="background-image: url('.$articles->urlToImage.');" title="'.$articles->description.'" href="'.$articles->url.'" class="news_card"><div class="title">'.$articles->title.'</div></a>';
-                }
+                
             }
         }
     }
